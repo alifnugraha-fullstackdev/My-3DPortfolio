ee4dc47e046556f53b2b558f53d847f12f803108
Muhammad Alif Maulana Nugraha <134056341+Akararasa12@users.noreply.github.com>
2026-04-29 01:04:46 +0700

    Hardcode EmailJS credentials to fix Netlify env var issues

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -50,8 +50,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_l9paqlq",
+        "template_8jsut5e",
         {
           from_name: form.name,
           to_name: "Alif Nugraha",
@@ -60,7 +60,7 @@ const Contact = () => {
           message: form.message,
           sent_date: new Date().toLocaleString(),
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "-vIO64HPzThWQEURx"
       )
       .then(
         () => {
